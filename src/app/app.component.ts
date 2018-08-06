a09f5b3a61ae5856bde9a108434134d1c1366227
“solittletime” <solittletime@users.noreply.github.com>
2018-08-06 14:09:14 -0400

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
 
+import { scheduleMap } from './app.constants';
+
 @Component({
   selector: 'app-root',
-  template: '',
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+  name = 'John Doe';
+  message = 'Please Clock In';
+  currentSchedule = 'Hover to see selected work times';
+
+  clockIn(event: MouseEvent) {
+    if (event.shiftKey) {
+      this.message = 'Clocked in as manager!';
+    } else {
+      this.message = 'Clocked in as employee';
+    }
+  }
+
+  clockOut() {
+    this.message = 'Have a nice day!';
+  }
+
+  showSchedule(day: string) {
+    this.currentSchedule = scheduleMap[day];
+  }
+
+  clearSchedule() {
+    this.currentSchedule = 'Hover to see selected work times';
+  }
 }
