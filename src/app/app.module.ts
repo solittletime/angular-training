c6ee51415b3c62e796116f84e8fef3be323387da
“solittletime” <solittletime@users.noreply.github.com>
2018-08-06 15:00:15 -0400

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { OrderManagerComponent } from './order-manager/order-manager.component';
+import { OrderListComponent } from './order-list/order-list.component';
+import { OrderDetailsComponent } from './order-details/order-details.component';
+import { OrderHeaderComponent } from './order-header/order-header.component';
+import { OrderItemsComponent } from './order-items/order-items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderManagerComponent,
+    OrderListComponent,
+    OrderDetailsComponent,
+    OrderHeaderComponent,
+    OrderItemsComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  imports: [BrowserModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
