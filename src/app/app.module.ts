268548f6de50f2f8aaa8d4303f5406d0cf7ca0f9
“solittletime” <solittletime@users.noreply.github.com>
2018-08-07 07:24:04 -0400

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
+import { CounterDisplayComponent } from './counter-display.component';
+import { appleCounterReducer, AppState, berryCounterReducer } from './state';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterDisplayComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot<AppState>({
+      berryCounter: berryCounterReducer,
+      appleCounter: appleCounterReducer
+    })
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
