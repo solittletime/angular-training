ef1f65faf4d9636a1cb8e8f39586d9ab9869acff
“solittletime” <solittletime@users.noreply.github.com>
2018-08-07 08:36:05 -0400

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { SafeHtml, DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
-  template: '',
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+  stringWithHtml: string;
+  htmlProperty: SafeHtml;
+
+  constructor(private _sanitizer: DomSanitizer) {
+    this.stringWithHtml = `
+      <button onClick="console.log('hello from old-school HTML/JS');">
+        Press Me
+      </button>
+      <i>Hello from <b>HTML</b></i>
+    `;
+    this.htmlProperty = this._sanitizer.bypassSecurityTrustHtml(this.stringWithHtml);
+  }
 }
