6ada2d78fd218db8dcd7c555947a1c4e18a6962a
“solittletime” <solittletime@users.noreply.github.com>
2018-08-07 08:33:35 -0400

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GreenWrapperComponent } from './green-wrapper.component';
+import { BlueWrapperComponent, BlueWrapperDirective } from './blue-wrapper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GreenWrapperComponent,
+    BlueWrapperComponent,
+    BlueWrapperDirective
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  imports: [BrowserModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
