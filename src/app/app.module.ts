81cddd6c5f9509eaaf4bf0549bb50f63f3e3e590
“solittletime” <solittletime@users.noreply.github.com>
2018-08-07 08:39:31 -0400

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToCapsPipe, ContainsXPipe, CheckmarkPipe, FieldRangePipe } from './pipes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToCapsPipe,
+    ContainsXPipe,
+    CheckmarkPipe,
+    FieldRangePipe
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  imports: [BrowserModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
