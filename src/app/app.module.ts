731661be2a3ae1ed5f207850db47d4f3c2ba6bba
“solittletime” <solittletime@users.noreply.github.com>
2018-08-07 09:19:11 -0400

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import { NewsComponent } from './news';
+import { CollectionPanelComponent, CollectionPanelComponentTagsDirective } from './collection-panel';
+import { AnnouncementComponent, AnnouncementComponentTagsDirective } from './announcement';
 
 @NgModule({
   declarations: [
-    AppComponent
+    NewsComponent,
+    CollectionPanelComponent,
+    CollectionPanelComponentTagsDirective,
+    AnnouncementComponent,
+    AnnouncementComponentTagsDirective
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, FormsModule],
+  bootstrap: [NewsComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
