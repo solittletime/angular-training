20c7441ddd0653ad7701c83a8caea1891ec3a4cd
“solittletime” <solittletime@users.noreply.github.com>
2018-08-07 08:31:25 -0400

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BlinkDirective } from './blink.directive';
+import { BounceDirective } from './bounce.directive';
+import { DragDirective } from './drag.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlinkDirective,
+    BounceDirective,
+    DragDirective
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  imports: [BrowserModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
