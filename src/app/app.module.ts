a46938fc6edc2b2311949c1ec63bd33464739bad
“solittletime” <solittletime@users.noreply.github.com>
2018-08-07 07:38:56 -0400

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { RedditSearchComponent } from './reddit-search/reddit-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RedditSearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
