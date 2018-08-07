8b4387ce5261a4d30e76ea82db20c76f8b81e4c9
“solittletime” <solittletime@users.noreply.github.com>
2018-08-07 07:41:32 -0400

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { EmployeeFilterComponent } from './employee-filter/employee-filter.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
+import { EmployeeListHeaderComponent } from './employee-list-header/employee-list-header.component';
+
+const routes: Routes = [
+  { path: '', component: EmployeeDashboardComponent}
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes, { useHash: true }),
+    HttpClientModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    EmployeeFilterComponent,
+    EmployeeListComponent,
+    EmployeeListHeaderComponent,
+    EmployeeDashboardComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
