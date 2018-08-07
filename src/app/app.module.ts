0f8f2ad2675b1dc72edef9a364912201248efe53
“solittletime” <solittletime@users.noreply.github.com>
2018-08-07 09:32:59 -0400

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
-import { AppComponent } from './app.component';
+import { ActivityPanelComponent } from './activityPanel';
+import { PersonListComponent } from './personList';
+import { OnePersonComponent } from './onePerson';
+import { ExampleComponent } from './example';
+
+export const firebaseConfig = {
+  apiKey: '<your-key>',
+  authDomain: '<your-project-authdomain>',
+  storageBucket: '<your-storage-bucket>',
+  messagingSenderId: '<your-messaging-sender-id>',
+
+  databaseURL: 'https://a2-fb-demo.firebaseio.com/'
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [ExampleComponent, ActivityPanelComponent, PersonListComponent, OnePersonComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ExampleComponent]
 })
 export class AppModule { }
