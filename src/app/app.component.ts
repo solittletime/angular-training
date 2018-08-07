49bd4b505444822bf8b5e26ae6f5dc6104fc321f
“solittletime” <solittletime@users.noreply.github.com>
2018-08-07 07:47:15 -0400

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,38 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { simpleAsyncValidator, westernZipValidatorFactory, slowAsyncValidator } from './asyncValidators';
 
 @Component({
   selector: 'app-root',
-  template: '',
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+  inputFormGroup: FormGroup;
+
+  constructor(http: HttpClient, fb: FormBuilder) {
+    this.inputFormGroup = fb.group({
+      input: ['', Validators.nullValidator, simpleAsyncValidator],
+      zip: [
+        '',
+        [
+          Validators.minLength(5),
+          Validators.maxLength(5),
+          Validators.required
+        ],
+        [
+          slowAsyncValidator,
+          westernZipValidatorFactory(http)
+        ]
+      ]
+    });
+  }
+
+  onLogin(): void {
+    console.log('logged in', this.inputFormGroup.value);
+  }
+
+  logTheForm(): void {
+    console.log('form', this.inputFormGroup);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import { AppComponent }  from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
