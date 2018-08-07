c1dbcb85dfac85e5b51dc95bef9abaeff32bcdd4
“solittletime” <solittletime@users.noreply.github.com>
2018-08-07 07:48:39 -0400

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '',
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NameComponent, HomeComponent, BlueBoxComponent, ChatComponent, TaskComponent } from './components';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent},
+  { path: 'hello/:name', component: NameComponent },
+  { path: 'blue', component: BlueBoxComponent },
+
+  { path: 'chat', component: ChatComponent, outlet: 'side' },
+  { path: 'task', component: TaskComponent, outlet: 'side' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NameComponent,
+    BlueBoxComponent,
+    ChatComponent,
+    TaskComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes, { useHash: true })
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
