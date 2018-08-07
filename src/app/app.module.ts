7172043dd562fab9a190f56abc483bff313efcf6
“solittletime” <solittletime@users.noreply.github.com>
2018-08-07 08:55:57 -0400

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { IfElseComponent } from './if-else.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [IfElseComponent],
+  imports: [BrowserModule],
+  bootstrap: [IfElseComponent]
 })
 export class AppModule { }
