7610c1aecc3efdaf0a25a82dd1fd460bde496de6
“solittletime” <solittletime@users.noreply.github.com>
2018-08-07 07:56:45 -0400

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { EmployeesService } from './employees.service';
 
 @Component({
   selector: 'app-root',
-  template: '',
+  template: `
+    <h3>Employees</h3>
+    <ul>
+      <li *ngFor="let e of employees | async">
+        {{e.first_name}} {{e.last_name}}
+      </li>
+    </ul>
+  `
 })
 export class AppComponent {
-  title = 'app';
+  employees: Observable<any[]>;
+
+  constructor(es: EmployeesService) {
+    // this.employees = es.getEmployees();
+    // this.employees = es.poll1();
+    this.employees = es.poll2();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppComponent } from './app.component';
+import { AppComponent }  from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
