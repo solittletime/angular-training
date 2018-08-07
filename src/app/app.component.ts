730f65df67fe1562212462f7a8b175edd1b17c55
“solittletime” <solittletime@users.noreply.github.com>
2018-08-07 08:30:12 -0400

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+
+import { ItemDataService } from './item-data/item-data.service';
 
 @Component({
   selector: 'app-root',
-  template: '',
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+  configFormGroup: FormGroup;
+
+  constructor(public ids: ItemDataService, fb: FormBuilder) {
+    this.configFormGroup = fb.group({
+      data: ['mutable'],
+      strategy: ['default']
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { OnPushStrategyComponent } from './display/onpush-strategy.component';
+import { DefaultStrategyComponent } from './display/default-strategy.component';
+import { ItemDetailComponent } from './display/item-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultStrategyComponent,
+    OnPushStrategyComponent,
+    ItemDetailComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  imports: [BrowserModule, ReactiveFormsModule],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
